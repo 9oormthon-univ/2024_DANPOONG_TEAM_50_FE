24ae3a9abd1414f47f452275f349501550feefaa
승우 <hsu235@naver.com>
2024-11-22 17:47:25 +0900

    App.js 파일 변경
    App.js 파일 변경

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Main from './pages/Main/Main'
+import Main2 from './pages/Main/Main2'
 import NavBar from './components/Nav/Navbar';
 import Search from './pages/Search/Search';
 import Signin from "./pages/Signin/Signin";
 
 import SearchPart from './pages/Search/SearchPart';
+import Map from './pages/Map/Map';
 
 const App = () => {
   return (
@@ -13,9 +15,14 @@ const App = () => {
           <div className="app-container">
               <Routes>
                   <Route path='/' element={<Main />} />
+                  <Route path='/donation' element={<Main2 />} />
                   <Route path='/Search' element={<Search />} />
                   <Route path='/Searchpart' element={<SearchPart />} />
+<<<<<<< HEAD
                   <Route path="/Signin" element={<Signin />} />
+=======
+                  <Route path='/Map' element={<Map />} />
+>>>>>>> main
               </Routes>
               <NavBar />
           </div>
