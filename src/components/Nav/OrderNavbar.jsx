a0a9092da1a9a1ceea25631168430ff7dd37b1b4
jllee000 <101315059+jllee000@users.noreply.github.com>
2024-11-23 10:27:24 +0900

    feat : 주문 헤더 뒤로가기 로직

--- src/components/Nav/OrderNavbar.jsx
+++ src/components/Nav/OrderNavbar.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import AlarmIcon from "../../assets/img/Order/alarm.png";
 import BackIcon from "../../assets/img/Nav/back.png";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 const OrderNavbar = ({ text }) => {
+  const navigate = useNavigate();
   return (
     <div>
       <div className="order-header">
-        <Link to="/">
-          <img src={BackIcon} className="back-img" alt="img" />
-        </Link>
+        <img
+          src={BackIcon}
+          className="back-img"
+          alt="img"
+          onClick={() => navigate(-1)}
+        />
+
         <span className="header-title">{text}</span>
         <img src={AlarmIcon} className="alarm-img" alt="img" />
       </div>
