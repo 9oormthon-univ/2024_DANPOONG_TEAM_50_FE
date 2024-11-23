33089e6bf7787f2c4017525ca2480d08e28af506
jllee000 <101315059+jllee000@users.noreply.github.com>
2024-11-23 23:55:38 +0900

    feat: kakao 로그인 리다이렉션

---
+++ src/pages/Signin/Redirection.jsx
@@ -0,0 +1,48 @@
+import { React, useEffect } from "react";
+
+export default function Redirection() {
+  const logincode = new URL(window.location.href).searchParams.get("code");
+  useEffect(() => {
+    if (logincode) {
+      sendLoginRequest();
+    }
+  }, []);
+
+  const sendLoginRequest = async () => {
+    try {
+      const response = await fetch(
+        `https://api.mymoo.site/api/v1/oauth/kakao/callback?code=${logincode}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+
+          credentials: "include",
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Received data:", data);
+        const accessToken = data.accessToken;
+        const refreshToken = data.refreshToken;
+
+        // setLoginSuccess(true);
+        // setIsNewMember(data.isNewMember);
+
+        localStorage.setItem("accessToken", accessToken);
+        localStorage.setItem("refreshToken", refreshToken);
+      } else {
+        console.error("Network response was not ok");
+      }
+    } catch (error) {
+      console.error("Login request error:", error);
+    }
+  };
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
