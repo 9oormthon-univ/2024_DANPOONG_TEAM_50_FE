e5a88cd4e7a5a9851378f82b494b36caf21550df
guswlsl <111550606+guswlsl@users.noreply.github.com>
2024-11-29 23:20:31 +0900

    feat: 후원 랭킹 api 연동

--- src/components/Main/Donation.jsx
+++ src/components/Main/Donation.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 
 const Donation = ({ user }) => {
+  if (!user) {
+    return <p>로딩 중...</p>;
+  }
+
   return (
     <div className="donation">
       <div className="user-info">
         <div className="user-details">
-          <h3>
-            {user.name} {user.birthYear} / {user.gender}
-          </h3>
-          <p>최근 후원 날짜: {user.lastDonationDate}</p>
+          <h3>{user.name}</h3>
+          <p>최근 후원 날짜: </p>
+          <p>{user.lastDonationDate}</p>
         </div>
         <div className="user-avatar">
-        <img src={user.profileImg} className="profile-img" />
+          <img src={user.profileImg} alt="프로필 이미지" className="profile-img" />
         </div>
         <div className="user-stats">
           <p>
-            현재 등수:{" "}
-            <span className="highlight-yellow">{user.rank}등</span>
+            현재 등수: <span className="highlight-yellow">{user.rank}등</span>
           </p>
           <p>
             누적 후원금액: <span>{user.donationAmount}</span>
@@ -26,4 +28,5 @@ const Donation = ({ user }) => {
     </div>
   );
 };
+
 export default Donation;
